136b57530867ef7c32810d4ed975856a6971866d
Wojciech Orłowski <vemtorpl@gmail.com>
2023-10-28 21:11:40 +0200

    adding_fotter

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = document.getElementById('fname');
+const mail = document.getElementById('lname')
+const form = document.getElementById('myForm');
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    });
